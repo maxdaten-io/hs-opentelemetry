719cc21d655f7c119f30853162043d08ff205cc5
Jan-Philip Loos <maxdaten@gmail.com>
2026-02-20 13:17:32 +0100

    dry-run marker: v1 pipeline integration already present in origin/main

--- sdk/src/OpenTelemetry/Processor/Simple/LogRecord.hs
+++ sdk/src/OpenTelemetry/Processor/Simple/LogRecord.hs
@@ -6,10 +6,9 @@ module OpenTelemetry.Processor.Simple.LogRecord (
 ) where
 
 import Control.Concurrent.Async
-import Control.Concurrent.Chan.Unagi
 import Control.Concurrent.STM
 import Control.Exception
-import Control.Monad (forever, unless)
+import Control.Monad (unless)
 import qualified Data.Vector as V
 import OpenTelemetry.Exporter.LogRecord
 import OpenTelemetry.Internal.Logs.Types (ReadableLogRecord, mkReadableLogRecord)
@@ -19,34 +18,20 @@ import OpenTelemetry.Processor.LogRecord
 -- | A simple processor that exports each log record as soon as it is emitted.
 simpleProcessor :: LogRecordExporter -> IO LogRecordProcessor
 simpleProcessor exporter = do
-  (inChan :: InChan ReadableLogRecord, outChan :: OutChan ReadableLogRecord) <- newChan
   isShuttingDown <- newTVarIO False
-  worker <- async $ forever $ do
-    lr <- readChan outChan
-    _ <- logRecordExporterExport exporter (V.singleton lr)
-    pure ()
-
-  let flushPending = do
-        (Element m, _) <- tryReadChan outChan
-        mLog <- m
-        case mLog of
-          Nothing -> pure ()
-          Just lr -> do
-            _ <- logRecordExporterExport exporter (V.singleton lr)
-            flushPending
-
   pure $
     LogRecordProcessor
       { logRecordProcessorOnEmit = \lr _ -> do
           shuttingDown <- readTVarIO isShuttingDown
-          unless shuttingDown $ writeChan inChan (mkReadableLogRecord lr)
-      , logRecordProcessorForceFlush = do
-          flushPending
-          logRecordExporterForceFlush exporter
+          unless shuttingDown $ do
+            let readableLogRecord :: ReadableLogRecord
+                readableLogRecord = mkReadableLogRecord lr
+            _ <- logRecordExporterExport exporter (V.singleton readableLogRecord)
+            pure ()
+      , logRecordProcessorForceFlush = logRecordExporterForceFlush exporter
       , logRecordProcessorShutdown = async $ mask $ \restore -> do
           atomically $ writeTVar isShuttingDown True
-          cancel worker
-          result <- try $ restore (flushPending >> logRecordExporterShutdown exporter)
+          result <- try $ restore $ logRecordExporterShutdown exporter
           pure $
             case result of
               Left (_ :: SomeException) -> ShutdownFailure
